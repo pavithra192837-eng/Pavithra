70a279754acffb9ccd79dd9ac0549a4c9c0e6e0d
pavithra192837-eng <pavithra192837@gmail.com>
2026-03-11 12:06:30 +0530

    Create pythonprogram.py

---
+++ pythonprogram.py
@@ -0,0 +1,10 @@
+import os
+
+# Clone the GitHub repository
+os.system("git clone https://github.com/username/repository.git")
+
+# Go inside the repository folder
+os.chdir("repository")
+
+# Run the python file
+os.system("python main.py")
